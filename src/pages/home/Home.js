60dfbb2ffbbe2da17f49e95ee512c785be7b2ec5
Hitha <86621583+Hitha95@users.noreply.github.com>
2021-10-25 09:20:35 +0530

    initial commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div>top</div>
+      <div>expenseList</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/redux/reducers/expenses.js
@@ -0,0 +1,10 @@
+const initialState = {
+  expenseList: [],
+};
+
+export const expenseReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
